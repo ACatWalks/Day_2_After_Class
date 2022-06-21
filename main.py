5186c48e8503b0351a0466d1043641736d09b4cf
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-06-20 23:40:28 -0700

    Pascal now updated

--- main.py
+++ main.py
@@ -23,29 +23,26 @@ def num_within(input, min, max):
     return input >= min and input <= max
 
 def pascal(n):
-    triangle = [[1], [1,1]]
     if n < 1:
-        print('Invalid number of rows')
+        print('Minimum number of rows is 1')
     elif n == 1:
-        print(1)
+        return 1
     else:
-        #No idea what to do from here: checked solution code 
-        row_number = 2
-        while len(triangle) < n:
+        triangle = [[1], [1,1]]
+        for i in range (2, n+1):
             row = []
-            row_prev = triangle[row_number - 1]
-            length = len(row_prev) + 1
-            for i in range(length):
-                if i == 0:
+            for j in range(0, i):
+                if j == 0:
                     row.append(1)
-                elif i > 0 and i < length - 1:
-                    row.append(triangle[row_number - 1][i-1] + triangle[row_number - 1][i])
+                elif j == i-1:
+                    row.append(1)
                 else:
-                    row.append(1)
+                    row.append(triangle[i-1][j] + triangle[i-1][j-1])
             triangle.append(row)
-            row_number += 1
         for row in triangle:
             print(row)
+
+
 
 print(max_num([3, 33, 333]))
 print(mult_list([2,4,6]))
